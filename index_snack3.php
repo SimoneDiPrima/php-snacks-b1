9c9ba58a0debd793346b8599b0ca96aac60332e1
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-08-30 21:38:04 +0200

    esercizio snack3

---
+++ index_snack3.php
@@ -0,0 +1,63 @@
+<?php
+
+$posts = [
+
+    '01/01/2012' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2022' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2017' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php for( $i=0 ; $i < count($posts) ; $i++ ) :?>
+        <li>
+            <?php for($j=0; $j<count($posts[$i]) ;$i++) :?>
+                <h4><?php $posts[$i][$j] ?></h4>
+                
+        </li>
+        <?php endfor; ?>
+        <?php endfor; ?>
+    </ul>
+</body>
+</html>
